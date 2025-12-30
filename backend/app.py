a2144012d0e74aa24a022a73308341153b8384be
pmarceaujr <paul@themarceaus.net>
2025-12-30 11:09:01 -0500

    Prepare Flask backend for Heroku

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,14 @@ import os
 app = create_app()
 
 
-# Serve React build in production
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_react(path):
-    if path != "" and os.path.exists(os.path.join('../frontend/build', path)):
-        return send_from_directory('../frontend/build', path)
-    else:
-        return send_from_directory('../frontend/build', 'index.html')
+# # Serve React build in production
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def serve_react(path):
+#     if path != "" and os.path.exists(os.path.join('../frontend/build', path)):
+#         return send_from_directory('../frontend/build', path)
+#     else:
+#         return send_from_directory('../frontend/build', 'index.html')
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
