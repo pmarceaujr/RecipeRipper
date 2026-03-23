6a26127588c86a80ba495673339f0f488a9cb852
pmarceaujr <paul@themarceaus.net>
2026-03-23 11:23:18 -0400

    Adding custom alert messages

--- frontend/src/utils/alerts.js
+++ frontend/src/utils/alerts.js
@@ -1,35 +1,35 @@
-// helpers/alerts.js
-export const showAlert = async ({
-  title = 'Notice',
-  text = '',
-  html = '',
-  icon = 'info',
-  confirmButtonText = 'OK',
-  showCancelButton = false,
-  cancelButtonText = 'Cancel',
-  onConfirm = () => {},
-  onCancel = () => {},
-  timer = null,
-  allowOutsideClick = true,
-} = {}) => {
-  const result = await Swal.fire({
-    title,
-    text,
-    html,
-    icon,
-    confirmButtonText,
-    showCancelButton,
-    cancelButtonText,
-    timer,
-    allowOutsideClick,
-    reverseButtons: showCancelButton,
-  });
+// // helpers/alerts.js
+// export const showAlert = async ({
+//   title = 'Notice',
+//   text = '',
+//   html = '',
+//   icon = 'info',
+//   confirmButtonText = 'OK',
+//   showCancelButton = false,
+//   cancelButtonText = 'Cancel',
+//   onConfirm = () => {},
+//   onCancel = () => {},
+//   timer = null,
+//   allowOutsideClick = true,
+// } = {}) => {
+//   const result = await Swal.fire({
+//     title,
+//     text,
+//     html,
+//     icon,
+//     confirmButtonText,
+//     showCancelButton,
+//     cancelButtonText,
+//     timer,
+//     allowOutsideClick,
+//     reverseButtons: showCancelButton,
+//   });
 
-  if (result.isConfirmed) {
-    onConfirm();
-  } else if (result.dismiss === Swal.DismissReason.cancel) {
-    onCancel();
-  }
+//   if (result.isConfirmed) {
+//     onConfirm();
+//   } else if (result.dismiss === Swal.DismissReason.cancel) {
+//     onCancel();
+//   }
 
-  return result;
-};
+//   return result;
+// };
