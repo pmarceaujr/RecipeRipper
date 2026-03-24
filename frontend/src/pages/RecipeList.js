09caac785227c42ee8e8c208250fc813d1bdb711
pmarceaujr <paul@themarceaus.net>
2026-03-24 09:14:32 -0400

    Added 7 as a page size option

--- frontend/src/pages/RecipeList.js
+++ frontend/src/pages/RecipeList.js
@@ -577,6 +577,7 @@ const handleLogout = () => {
                         style={{ padding: '6px', fontSize: '1rem', borderRadius: '4px' }}
                       >
                         <option value={5}>5</option>
+                        <option value={7}>7</option>
                         <option value={10}>10</option>
                         <option value={15}>15</option>
                         <option value={20}>20</option>
