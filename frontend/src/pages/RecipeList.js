aaba8e9f057d8f3a8b5aa7af2439e447d267b3ed
pmarceaujr <paul@themarceaus.net>
2026-03-24 09:24:11 -0400

    Added 8 and 9 as a page size option

--- frontend/src/pages/RecipeList.js
+++ frontend/src/pages/RecipeList.js
@@ -578,6 +578,8 @@ const handleLogout = () => {
                       >
                         <option value={5}>5</option>
                         <option value={7}>7</option>
+                        <option value={8}>8</option>
+                        <option value={9}>9</option>
                         <option value={10}>10</option>
                         <option value={15}>15</option>
                         <option value={20}>20</option>
