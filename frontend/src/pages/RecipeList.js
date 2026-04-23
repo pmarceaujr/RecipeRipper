ec13c925bc2fdaac15ab1a625e2bc7d59b9e0614
pmarceaujr <codemaster@themarceaus.net>
2026-04-23 14:51:11 -0400

    Updated release date and time

--- frontend/src/pages/RecipeList.js
+++ frontend/src/pages/RecipeList.js
@@ -403,7 +403,7 @@ const handleLogout = () => {
       <header className="App-header">
         <div className="header-content">
 
-          <h1>🍳 The Recipe Ripper Database <span style={{ fontSize: ".27EM" }}>v:7.5.2 - 04-23-2026:12:21</span></h1> 
+          <h1>🍳 The Recipe Ripper Database <span style={{ fontSize: ".27EM" }}>v:7.5.2 - 04-23-2026:14:50</span></h1> 
           <button className="auth-button"
             onClick={isLoggedIn ? handleLogout : handleLogin}
         >
